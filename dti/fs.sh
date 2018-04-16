6db3b3cb584873c2536b449999be720a7e03944e
rob.mccutcheon1@gmail.com <robmcc@Roberts-MacBook-Pro.local>
2018-04-16 12:10:01 +0100

    making queue script

--- dti/fs.sh
+++ dti/fs.sh
@@ -8,8 +8,11 @@ export struc_dir="/data/project/AMPH/rob/dti"
 
 cd $SUBJECTS_DIR/..
 
+#wait for structural_reg to be created by mri.sh
+while [ ! -f $struc_dir/$subject_id/$session_id/'structural_reg.nii' ]; do sleep 1; done
+
 #run freesurfer recon-all on structural registred to B0s
-#recon-all -sd $subject_id -s $session_id -i $struc_dir/$subject_id/$session_id/'structural_reg.nii' -all
+recon-all -sd $subject_id -s $session_id -i $struc_dir/$subject_id/$session_id/'structural_reg.nii' -all
 
 #run easy_lausanne (needs python2, fsl 5.0.9, freesurfer6.0.0)
 mkdir $SUBJECTS_DIR/$session_id/'lausanne'
@@ -20,3 +23,10 @@ easy_lausanne\
     --target_type diffusion \
     --output_dir $SUBJECTS_DIR/$session_id/lausanne \
     --include500
+
+#dilate easy_lausanne atlases
+resolutions='33 60 125 250 500'
+for resolution in $resolutions
+do
+    atlas_dilate 'ROIv_scale'$resolution'.nii.gz' 'ROIv_scale'$resolution'_thick.nii.gz'
+done
